395f743209a6320b8ef1a4bf321d670d25f1d36e
Pranjal Mittal <mittal.pranjal@gmail.com>
2014-03-19 18:04:45 +0530

    Added a change_module view that lets to change rsyncd config file over HTTP.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 import simplejson as json
 import ConfigParser
 
+RSYNCD_CONF_FILE = 'rsyncd-osl.conf'
 
 class DummyGlobalSectionHead(object):
     '''
@@ -35,9 +36,9 @@ def sections_as_dict(cp):
 
 
 cp = ConfigParser.ConfigParser()
-cp.readfp(DummyGlobalSectionHead(open('rsyncd-osl.conf')))
+cp.readfp(DummyGlobalSectionHead(open(RSYNCD_CONF_FILE)))
 
-# Get all the sections in the rsyncd config file.
+# Get all the sections in the rsyncd config file with other data.
 sections = sections_as_dict(cp)
 
 # Get all the modules in the rsyncd config file.
@@ -47,13 +48,32 @@ modules = sections.keys()
 # global_items = cp.items('global')
 
 
-@app.route('/json/sections', methods=['GET'])
+@app.route('/json/sections/', methods=['GET'])
 def json_sections():
     '''
     Returns the sections of a rsyncd configuration file as a json response
     '''
     json_response = jsonify(sections)
     return json_response
+
+
+@app.route('/addparam/', methods=['GET',])
+def change_module():
+    '''
+    Change an existing rsync module by adding more key value pairs.
+    Usage: /addparam/?module=<module_name>&key=<key>&value=<value>
+    '''
+    module = request.args["module"]
+    key = request.args["key"]
+    value = request.args["value"]
+
+    cp.set(module, key, value)
+
+    # Write the modified configuration back to the file.
+    with open(RSYNCD_CONF_FILE, 'w') as configfile:    # save
+        cp.write(configfile)
+
+    return jsonify({"success": True})
 
 
 if __name__ == "__main__":
