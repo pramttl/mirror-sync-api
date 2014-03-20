ae7027022c67c3ce0ec5e3f03aaf491ca484f8cc
Pranjal Mittal <mittal.pranjal@gmail.com>
2014-03-21 01:13:35 +0530

    Switched to json based post.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
+import simplejson as json
+
 from apscheduler.scheduler import Scheduler
 LOG_FILE = "logfile.txt"
 
@@ -16,8 +18,7 @@ sched.start()
 import logging
 
 
-def sync_project_from_upstream(project, host, source, dest,
-                               rsync_password, start_date):
+def sync_project_from_upstream(project, host, source, dest, rsync_password):
     # Todo
     '''
     args = ["rsync", "-avz", "--include='*/'", source, dest]
@@ -33,22 +34,22 @@ def add_project():
 
     @project: Name of the project
     @host: IP or hostname of the upstream rsync provider.
-    @source_path: Complete path on the source that tells the rsync path.
+    @source: Complete path on the source that tells the rsync path.
     '''
-    project = request.form["project"]
-    host = request.form["host"]
-    source = request.form["source"]
-    dest = request.form["dest"] or "/data/ftp/.1"
-    interval_unit = request.form["interval_unit"] or "minutes"
-    interval = int(request.form["interval"])
-    start_date = request.args["start_date"] or None
-    rsync_password = ""
+    project_obj = request.json
+    project = project_obj["project"]
+
+    print project_obj
 
     job_function = lambda: sync_project_from_upstream(project,
-                                                      host,
-                                                      source_path,
-                                                      interval,
-                                                      start_date)
+                                                      project_obj["host"],
+                                                      project_obj["source"],
+                                                      project_obj["dest"],
+                                                      project_obj["rsync_password"])
+
+    start_date = project_obj.get("start_date")
+    interval = int(project_obj.get("interval")) or 1
+    interval_unit = project_obj.get("interval_unit", "minutes")
 
     if start_date:
         interval_kwargs = {"start_date": start_date, interval_unit: interval}
@@ -60,7 +61,7 @@ def add_project():
     # Add the job to the already running scheduler.
     sched.add_interval_job(job_function, **interval_kwargs)
 
-    return jsonify({"success": True})
+    return jsonify({'method': 'add_project', 'success': True, "project": project})
 
 
 if __name__ == "__main__":
