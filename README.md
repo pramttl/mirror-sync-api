fef38942347bea2f780fe0da4af8c9d312256a0e
Pranjal Mittal <mittal.pranjal@gmail.com>
2014-03-20 23:15:02 +0530

    Added code for scheduling tasks in main.py

--- README.md
+++ README.md
@@ -1,29 +1,18 @@
-Proof of concept of mirror syncing API
---------------------------------------
+Open Source Lab: Simple Mirror Syncing API
+------------------------------------------
 
-This repostiory is an experimental proof of concept of an API that writes to
-configuration files. To setup do the following steps:
+The following functionality is being built at the moment:
 
-    virtualenv venv
-    pip install -r requirements.txt
-    python main.py
+* Adding projects
 
-    # This starts the local development api server
+### Adding a project
 
+Projects are added via the api. The following parameters are specified:
 
-####  Visit
-
-http://localhost:5000/json/sections
-
-This lists all the sections of the `rsyncd-osl.conf` file as a json response.
-This file is available in the root directory of this repository.
-
-Further, to add more configuration to an existing rsync module visit
-the following:
-
-    localhost:5000/addparam/?module=all&key=testkey&value=testvalue
-
-The `rsyncd-osl.conf` in the root directory is modified with the new key and
-value added to the file. There are some issues with writing the global section
-that will be corrected.
-
+* Project name
+* Host IP
+* Source (Absolute path on the host used to pull content)
+* Destination (Path on the master where the
+* interval_unit: {minutes, hours or days}
+* interval_value: an integer
+* start_date in posix format Eg: "2014-03-10 09:30"
--- main.py
+++ main.py
@@ -1,81 +1,42 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-import simplejson as json
-import ConfigParser
+from apscheduler.scheduler import Scheduler
 
-RSYNCD_CONF_FILE = 'rsyncd-osl.conf'
+# Configuring a persistent job store and instantiating scheduler
+config = {'apscheduler.jobstores.file.class': 'apscheduler.jobstores.shelve_store:ShelveJobStore',
+          'apscheduler.jobstores.file.path': 'scheduledjobs.db'}
 
-class DummyGlobalSectionHead(object):
+sched = Scheduler(config)
+
+def sync_project_from_upstream():
+    print "Syncing from upstream!"
+
+
+@app.route('/addproject/', methods=['POST',])
+def add_project():
     '''
-    Useful to a dummy global section on top of an ini file so that it is
-    parsable via ConfigParser.
+    Schedules an upstream project for syncing (periodic)
+
+    @project_name: Name of the project
+    @host: IP or hostname of the upstream rsync provider.
+    @source_path: Complete path on the source that tells the rsync path.
     '''
-    def __init__(self, fp):
-        self.fp = fp
-        self.sechead = '[global]\n'
-    def readline(self):
-        if self.sechead:
-            try: return self.sechead
-            finally: self.sechead = None
-        else: return self.fp.readline()
+    project_name = request.args["name"]
+    host = request.args["host"]
+    source_path = request.args["source_path"]
+    interval_unit = request.args["interval_unit"]
+    interval = int(request.args["interval"])
+    start_date = request.args["start_date"]
 
+    interval_kwargs = {'start_date': start_date, interval_unit: 2}
 
-def sections_as_dict(cp):
-    '''
-    Returns all the sections as a dictionary.
-    '''
-    d = dict(cp._sections)
-    for section in d:
-        for opt in d[section]:
-            if opt != '__name__':
-                d[section][opt] = cp.get(section, opt)
-        d[section] = dict(cp._defaults, **d[section])
-        d[section].pop('__name__', None)
-    return d
-
-
-cp = ConfigParser.ConfigParser()
-cp.readfp(DummyGlobalSectionHead(open(RSYNCD_CONF_FILE)))
-
-# Get all the sections in the rsyncd config file with other data.
-sections = sections_as_dict(cp)
-
-# Get all the modules in the rsyncd config file.
-modules = sections.keys()
-
-# To get items of a global section.
-# global_items = cp.items('global')
-
-
-@app.route('/json/sections/', methods=['GET'])
-def json_sections():
-    '''
-    Returns the sections of a rsyncd configuration file as a json response
-    '''
-    json_response = jsonify(sections)
-    return json_response
-
-
-@app.route('/addparam/', methods=['GET',])
-def change_module():
-    '''
-    Change an existing rsync module by adding more key value pairs.
-    Usage: /addparam/?module=<module_name>&key=<key>&value=<value>
-    '''
-    module = request.args["module"]
-    key = request.args["key"]
-    value = request.args["value"]
-
-    cp.set(module, key, value)
-
-    # Write the modified configuration back to the file.
-    with open(RSYNCD_CONF_FILE, 'w') as configfile:    # save
-        cp.write(configfile)
+    sched.add_interval_job(sync_project_from_upstream, **interval_kwargs)
+    sched.start()
 
     return jsonify({"success": True})
-
 
 if __name__ == "__main__":
     app.debug = True
     app.run()
+
